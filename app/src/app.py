065d3277d37760ae7ed62967290f49f28ad21941
Vidit Pawar <vidit.pawar25@gmail.com>
2026-04-09 19:37:03 -0700

    Set up basic Flask app

---
+++ app/src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify({
+        "message": "Live Cricket Score Monitoring API is running"
+    })
+
+@app.route("/health")
+def health():
+    return jsonify({
+        "status": "healthy"
+    })
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
